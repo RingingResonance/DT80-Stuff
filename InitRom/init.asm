6950cac0ceefc775b0275f8c27918245d34db855
perpetualwalnut <madeofsolder@gmail.com>
2022-05-10 18:40:06 -0500

    Fixed init rom so that it uses built in RS232 port of the DT80 at 9600 8N1

--- InitRom/init.asm
+++ InitRom/init.asm
@@ -5,8 +5,8 @@ org 0x0800
 	jmp init
 	hlt         ;External TX routine marker
 	jmp TX
-	nop         ;External RX routine marker
-	nop
+	hlt         ;External RX routine marker
+	jmp RX
 	nop
 	nop
 	nop
@@ -40,7 +40,7 @@ init:  mvi a,0x07  ;Turn down brightness of CRT
     out 0x10
     mvi a,0x14  ;Select T0 LSB, RX
     out 0x13
-    mvi a,0xC0  ;Load D192 for 9600BAUD
+    mvi a,0x0C  ;Load D12 for 9600BAUD
     out 0x10
     mvi a,0x64  ;Select T1 MSB, TX
     out 0x13
@@ -48,17 +48,19 @@ init:  mvi a,0x07  ;Turn down brightness of CRT
     out 0x11
     mvi a,0x54  ;Select T1 LSB, TX
     out 0x13
-    mvi a,0xC0  ;Load D192 for 9600BAUD
+    mvi a,0x0C  ;Load D12 for 9600BAUD
     out 0x11
     ;Configure USART
-    mvi a,0x8C  ;Configure serial port for 8N1 Internal Sync
+    mvi a,0x4E  ;Configure serial port for 8N1 Internal Sync
     out 0x01
     mvi a,0x00
+    out 0x01
+    mvi a,0x05  ;Enable transmit and receive.
     out 0x01
     ;Configure additional port attributes
     mvi a,0x00  ;Use external timer for BAUD
     out 0x3E
-;0x1000 through 0x17FF is the third ROM. We are going to copy text from that.
+;0x1000 through 0x17FF is the third ROM. We are going to copy text/data from that to ram.
     lxi h,0x1000    ;Start of 3rd ROM
     lxi d,0x8050    ;Start of memory to copy to.
 cpy: mov a,m
@@ -76,16 +78,27 @@ cpy: mov a,m
 	jmp 0x018F      ;Jump back to main program and continue.
 
 ;Hardware serial port routines.
+;Transmit
 TX: mov a,b
-    out 0x00    ;Load serial port with data byte.
-    mvi a,0x01
-    out 0x01    ;Tell it to transmit.
+    out 0x00    ;Load serial port with data byte. This should auto start transmitting.
 ;Loop until done transmitting.
 TL: in  0x01    ;Get status
-    ani 0x04    ;And it with 0x04
+    ani 0x01    ;And it with 0x01
     jz  TL      ;if not zero then it's done.
     pop h
     pop psw
+    pop d
+    pop b
+    ret
+
+;Receive
+RX: in  0x01    ;Get status
+    ani 0x02    ;And it with 0x02
+    jz  RX      ;if not zero then there's a data byte.
+;Loop until data is ready.
+;Once data is ready, just read it and it should be in REG A where it needs to be anyways.
+    in  0x00    ;Read serial port 1 RX data.
+    pop h
     pop d
     pop b
     ret
